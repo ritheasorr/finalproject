f145ab966a0da5c2ad7efb718d5aab73873117d4
ritheasor <ritheasorlol@gmail.com>
2024-10-27 23:22:38 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Starter Template',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Home(),
+    );
+  }
 }
-
 
